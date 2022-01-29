431a3235843b1114fef0e4ec32b73c1f1621dc19
Göker Tanrısever <gokertanrisever@gmail.com>
2022-01-29 16:48:56 +0300

    reservations component added

---
+++ client/src/components/Reservations/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './index.module.css';
+
+const Reservations = () => {
+  return (
+    <div className={styles.title}>Reservations</div>
+  );
+}
+
+export default Reservations;
